c8f55463b6471460629ee88884d41496619dfca2
BaiWolf <33189326+BaiWolf@users.noreply.github.com>
2021-04-21 11:53:00 -0300

    Add files via upload
    群晖和Openwrt的cloudflare DDNS脚本

---
+++ cf-v4-ddns.openwrt.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+
+# 脚本会自动获取你的公网IPv4或者IPv6并更新到CloudFlare对应的记录上
+# 能够自动获取你的Cloudflare Zone ID 和Record ID
+
+# 用法:
+# cf-ddns.sh -k <你的cloudflare api key> \
+#            -u <Cloudflare登录邮箱> \
+#            -h <host.example.com> \     # 你想要DDNS的完整域名
+#            -z <example.com> \          # 主域名，即二级域名，或者说站点名称
+#            -t <A|AAAA>                 # IPv4模式或者IPv6模式；默认IPv4
+
+# 可选参数:
+#            -f false|true \           # 强制更新记录，忽略本地ip文件
+
+##################################################################################
+# 以下为默认配置，在没有填写命令行参数的情况下有效。
+# 命令行参数会覆盖下面的配置
+
+# 你的Global API Key, 请见 https://dash.cloudflare.com/profile/api-tokens,
+# 如果填写错误会造成请求错误
+CFKEY="a819c7b3aceb362d0951e68829dccfe5b4645"
+
+# Cloudflare登录邮箱, 例如: user@example.com
+CFUSER="xxxxxx@gmail.com"
+
+# 主域名，即二级域名，或者说站点名称, 例如: example.com
+CFZONE_NAME="guan.com"
+
+# 想要进行ddns的域名, 例如: homeserver.example.com，也可以是二级域名如 example.com
+# 请分别设置用于IPv4 DDNS 和 IPv6 DDNS 的域名。当然，两者可以相同也可以其中一个不填（如果你用不着其中一项的话）
+CFRECORD_NAMEV4="www.guan.com"
+CFRECORD_NAMEV6="www.guan.com"
+
+# 记录类型, A(IPv4)|AAAA(IPv6), 默认 IPv4
+CFRECORD_TYPE="AAAA"
+
+# TTL设置, 在 120 和 86400 秒之间
+CFTTL=600
+
+# 忽略本地ip文件，强制更新记录
+FORCE=false
+
+# 用于获取公网IP的地址, 可以换成其他的比如: bot.whatismyipaddress.com, https://api.ipify.org/ ...
+# 请分别设置用于IPv4 DDNS 和 IPv6 DDNS 的参数。当然，两者可以相同也可以其中一个不填（如果你用不着其中一项的话）
+#https://ipv4.icanhazip.com   https://ipv6.icanhazip.com
+#http://ip.3322.net
+#http://members.3322.org/dyndns/getip
+#WANIPSITEV4="http://ip.3322.net"
+#WANIPSITEV6="http://ipv6.icanhazip.com" 
+#获取公网IPV4
+WANIPSITEV4="$(/sbin/ip -4 addr | grep inet | awk -F '[ \t]+|/' '{print $3}' | grep -v "^192\.168" | grep -v "^172\.1[6-9]" | grep -v "^172\.2[0-9]]" | grep -v "^172\.3[0-1]" | grep -v "^10" | grep -v "^127" | grep -m1 '')"
+echo "本机公网IPV4地址是："$WANIPSITEV4;
+WANIPSITEV6="$(/sbin/ip -6 addr | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^f | grep -m1 '')"
+echo "本机IPV地址是："$WANIPSITEV6;
+
+# 这个文件将会存储你的zoneid和recordid等信息，可以是绝对路径或者相对路径
+# 请分别设置用于IPv4 DDNS 和 IPv6 DDNS 的路径。当然，两者可以相同也可以其中一个不填（如果你用不着其中一项的话）
+ID_FILEV4="/var/log/cloudflare.v4.ids" 
+ID_FILEV6="/var/log/cloudflare.v6.ids"
+
+# 这个文件将会在每一次IPv4地址变更后存储下当前IP，作为对比
+# 请分别设置用于IPv4 DDNS 和 IPv6 DDNS 的路径。当然，两者可以相同也可以其中一个不填（如果你用不着其中一项的话）
+WAN_IP_FILEV4="/var/log/ipv4.txt"
+WAN_IP_FILEV6="/var/log/ipv6.txt"
+
+# 日志文件路径，分别是IPv4和IPv6，可以为同一个，不会互相覆盖
+LOG_FILEV4="/var/log/cf_ddns.log"
+LOG_FILEV6="/var/log/cf_ddns.log"
+
+# 配置部分结束
+#############################################################################
+
+log() {
+    if [ "$1" ]; then
+        echo -e "[$(date)] - $1" >> $LOG_FILE
+    fi
+}
+
+
+# 获取参数
+while getopts k:u:h:z:t:f: opts; do
+  case ${opts} in
+    k) CFKEY=${OPTARG} ;;
+    u) CFUSER=${OPTARG} ;;
+    h) CFRECORD_NAME=${OPTARG} ;;
+    z) CFZONE_NAME=${OPTARG} ;;
+    t) CFRECORD_TYPE=${OPTARG} ;;
+    f) FORCE=${OPTARG} ;;
+  esac
+done
+
+if [ "$CFRECORD_TYPE" = "A" ]; then
+  ID_FILE=$ID_FILEV4
+  WAN_IP_FILE=$WAN_IP_FILEV4
+  LOG_FILE=$LOG_FILEV4
+  CFRECORD_NAME=$CFRECORD_NAMEV4
+  WANIPSITE=$WANIPSITEV4
+  IP_TYPE="IPv4"
+elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
+  WANIPSITE=$WANIPSITEV6
+  ID_FILE=$ID_FILEV6
+  WAN_IP_FILE=$WAN_IP_FILEV6
+  LOG_FILE=$LOG_FILEV6
+  CFRECORD_NAME=$CFRECORD_NAMEV6
+  IP_TYPE="IPv6"
+else
+  echo "CFRECORD_TYPE参数错误，你填写的值是 $CFRECORD_TYPE ,它只能是 A(IPv4) 或者 AAAA(IPv6)"
+  log "CFRECORD_TYPE参数错误，你填写的值是 $CFRECORD_TYPE ,它只能是 A(IPv4) 或者 AAAA(IPv6) \n"
+  exit 2
+fi
+
+# 如果缺少必要的参数就退出
+if [ "$CFKEY" = "" ]; then
+  echo "缺少 Global API Key,前往 https://dash.cloudflare.com/profile/api-tokens 获取"
+  log "[$IP_TYPE]缺少 Global API Key,前往 https://dash.cloudflare.com/profile/api-tokens 获取"
+  echo "请把它保存在 ${0} 或使用 -k 参数"
+  log "[$IP_TYPE]请把它保存在 ${0} 或使用 -k 参数 \n"
+  exit 2
+fi
+if [ "$CFUSER" = "" ]; then
+  echo "缺少用户名,这应该是你用于登录Cloudflare的电子邮件地址"
+  log "[$IP_TYPE]缺少用户名,这应该是你用于登录Cloudflare的电子邮件地址"
+  echo "请把它保存在 ${0} 或使用 -u 参数"
+  log "[$IP_TYPE]请把它保存在 ${0} 或使用 -u 参数 \n"
+  exit 2
+fi
+if [ "$CFRECORD_NAME" = "" ]; then 
+  echo "缺少域名(CFRECORD_NAME), 你想要想要对哪个域名进行DDNS?"
+  log "[$IP_TYPE]缺少域名(CFRECORD_NAME), 你想要想要对哪个域名进行DDNS?"
+  echo "请把它保存在 ${0} 或使用 -h 参数"
+  log "[$IP_TYPE]请把它保存在 ${0} 或使用 -h 参数 \n"
+  exit 2
+fi
+# 如果CFRECORD_NAME不是全限定域名
+if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
+  CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
+  echo "主机名不是全限定域名，已自动补全"
+  log "[$IP_TYPE]主机名不是全限定域名，已自动补全"
+fi
+# 取得当前的&旧的公网IP，直接取本机IP地址
+#WAN_IP=`curl -s ${WANIPSITE}`
+WAN_IP=$WANIPSITE
+
+if [ "$WAN_IP" ]; then
+	echo "本机IPV6是"$WAN_IP;
+else
+	echo "没有公网IP"
+	exit
+fi
+
+if [ -f $WAN_IP_FILE ]; then
+  OLD_WAN_IP=`cat $WAN_IP_FILE`
+else
+  echo "无法找到旧的IP地址，似乎是第一次运行这个脚本？"
+  log "[$IP_TYPE]无法找到旧的IP地址，似乎是第一次运行这个脚本？"
+  OLD_WAN_IP=""
+fi
+# 如果公网IP没有变化就退出，避免过于频繁的调用API
+if [ "$WAN_IP" = "$OLD_WAN_IP" ] && [ "$FORCE" = false ]; then
+  echo "公网IP($OLD_WAN_IP)没有变化,如果要强制更新请使用\"-f true\"参数"
+  log "[$IP_TYPE]公网IP($OLD_WAN_IP)没有变化,如果要强制更新请使用\"-f true\"参数 \n"
+  exit 0
+fi
+# 取得zoneid和recordid
+
+if [ -f $ID_FILE ] && [ $(wc -l $ID_FILE | cut -d " " -f 1) == 4 ] \
+  && [ "$(sed -n '3,1p' "$ID_FILE")" == "$CFZONE_NAME" ] \
+  && [ "$(sed -n '4,1p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
+    CFZONE_ID=$(sed -n '1,1p' "$ID_FILE")
+    CFRECORD_ID=$(sed -n '2,1p' "$ID_FILE")
+else
+    echo "正在更新ZoneID和RecordID"
+	log "[$IP_TYPE]正在更新ZoneID和RecordID"
+    #CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | sed 's/,/\n/g' | grep "result\":\[{\"id" | sed 's/:/\n/g' | sed '1,2d' | sed 's/"//g')
+	CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | jsonfilter -e '@["result"][0].id')
+	echo "CFZONE_ID = $CFZONE_ID"
+	log "[$IP_TYPE]zoneid=$CFZONE_ID"
+    #CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME&type=$CFRECORD_TYPE" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | sed 's/,/\n/g' | grep "result\":\[{\"id" | sed 's/:/\n/g' | sed '1,2d' | sed 's/"//g')
+	CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME&type=$CFRECORD_TYPE" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | jsonfilter -e '@["result"][0].id')
+	echo "CFRECORD_ID = $CFRECORD_ID"
+	log "[$IP_TYPE]zoneid=$CFZONE_ID"
+	log "[$IP_TYPE]recordid=$CFRECORD_ID"
+    echo "$CFZONE_ID" > $ID_FILE
+    echo "$CFRECORD_ID" >> $ID_FILE
+    echo "$CFZONE_NAME" >> $ID_FILE
+    echo "$CFRECORD_NAME" >> $ID_FILE
+fi
+# 如果IP有变化就更新记录
+echo "本地保存的IP是 $OLD_WAN_IP ,正在更新DNS记录到 $WAN_IP"
+log "[$IP_TYPE]本地保存的IP是 $OLD_WAN_IP ,正在更新DNS记录到 $WAN_IP"
+
+RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
+  -H "X-Auth-Email: $CFUSER" \
+  -H "X-Auth-Key: $CFKEY" \
+  -H "Content-Type: application/json" \
+  --data "{\"id\":\"$CFZONE_ID\",\"type\":\"$CFRECORD_TYPE\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WAN_IP\", \"ttl\":$CFTTL}")
+if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
+  echo "成功！已将 $CFRECORD_NAME 的记录更新到 $WAN_IP"
+  log "[$IP_TYPE]成功！已将 $CFRECORD_NAME 的记录更新到 $WAN_IP \n"
+  #log "[$IP_TYPE][debug]Response:\n $RESPONSE"     #debug用
+  echo $WAN_IP > $WAN_IP_FILE
+  exit
+else
+  echo '似乎哪里出问题了 :(  API返回信息如下'
+  echo "$RESPONSE"
+  log '似乎哪里出问题了 :(  API返回信息如下'
+  log "[$IP_TYPE][debug]Response:\n $RESPONSE \n"
+  exit 1
+fi
